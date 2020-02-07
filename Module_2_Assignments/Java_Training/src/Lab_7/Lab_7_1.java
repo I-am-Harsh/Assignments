01065ec1d8a58bafacd400af96c24cc2681f9201
Harsh Verma <verma.harsh98@gmail.com>
2020-02-07 08:36:01 +0530

    init

--- Module_2_Assignments/Java_Training/src/Lab_7/Lab_7_1.java
+++ Module_2_Assignments/Java_Training/src/Lab_7/Lab_7_1.java
@@ -4,19 +4,19 @@ package Lab_7;
 // import java.collections.*;
 import java.util.*;
 
-public class Lab_6_7{
+public class Lab_7_1{
 
    
     public static void main(String args[]){
-        Scanner sc =  new Scanner(System.in);
+        // Scanner sc =  new Scanner(System.in);
 
-        String name = sc.nextLine();
-        sc.close();
+        // String name = sc.nextLine();
+        // sc.close();
 
-        HashMap hMap = new HashMap<Integer>();
-        hMap.add("asd");
-        hMap.add("ad");
-        hMap.add("sd");
+        HashMap<Integer,String> hMap = new HashMap<Integer,String>();
+        hMap.put(101,"asd");
+        hMap.put(103,"sd");
+        hMap.put(102,"ad");
 
         System.out.println(hMap);
     }
---
+++ Module_2_Assignments/Java_Training/src/Lab_7/Lab_7_2.java
@@ -0,0 +1,26 @@
+package Lab_7;
+
+
+// import java.collections.*;
+import java.util.*;
+
+public class Lab_7_2{
+
+   
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        
+        char arr[] = new char[number];
+
+        for(int i = 0; i < number; i++){
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        HashMap hMap = new HashMap<Character,Integer>();
+
+        for()
+    }
+}
