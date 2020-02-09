6ad4e5af8b94d9a2acd52531ba4520c84d195fec
Harsh Verma <verma.harsh98@gmail.com>
2020-02-09 17:14:26 +0530

    updated lab 4

--- Module_2_Assignments/Java_Training/src/Lab_4/Lab_4_1.java
+++ Module_2_Assignments/Java_Training/src/Lab_4/Lab_4_1.java
@@ -1,18 +1,28 @@
 package Lab_4;
+
+import java.util.*;
 public class Lab_4_1 {
 
 	public static void cubes(int number){
-		// int sum = 0;
+		// System.out.println(number);
+		int sum = 0;
 		String arr = String.valueOf(number);
-		for(int i = 0; i < arr.length() ; i++){
-			// int digit = 
-			// sum += Integer.parseInt(arr.charAt(i));
+		for(int i = 0; i < arr.length() ; i++){ 
+			int digit = Character.getNumericValue(arr.charAt(i));
+			// System.out.println(digit);
+			sum += digit * digit * digit;
 		}
+		System.out.println(sum);
 	}
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 
+		int number = sc.nextInt();
+		sc.close();
+
+		cubes(number);
 	}
 
 }
