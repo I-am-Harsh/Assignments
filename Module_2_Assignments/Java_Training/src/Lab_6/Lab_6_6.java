3777d0d54e81919d78e7261308bbee5cb7d978ee
Harsh Verma <verma.harsh98@gmail.com>
2020-02-09 17:22:12 +0530

    updated the time question

--- Module_2_Assignments/Java_Training/src/Lab_6/Lab_6_6.java
+++ Module_2_Assignments/Java_Training/src/Lab_6/Lab_6_6.java
@@ -1,30 +1,21 @@
 package Lab_6;
 
+import java.time.*;
 import java.util.*;
-import java.text.SimpleDateFormat;    
-import java.util.Date;     
 
-
-public class Lab_6_6{
-
-    // void getdate(String year,String month,String day){
-
-    // }
-    public static void main(String args[]){
+public class Lab_6_6 {  
+   public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter the date in the format DD MM YYYY");
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
-        Date date = new Date();  
-        System.out.println(formatter.format(date));   
-        
-        // String day = sc.nextLine();
-        // String month = sc.nextLine();
-        // String year = sc.nextLine();
-
-
+        System.out.println("Enter in the format DD MM YYYY and press enter after each input");
+        int day = sc.nextInt();
+        int month = sc.nextInt();
+        int year = sc.nextInt();
+        LocalDate pdate = LocalDate.of(year, month, day);
+        LocalDate now = LocalDate.now();
         sc.close();
-        
-
-    }
+        Period diff = Period.between(pdate, now);
+ 
+     System.out.printf("\nDifference is %d years, %d months and %d days old\n\n", diff.getYears(), diff.getMonths(), diff.getDays());
+  }
 }
